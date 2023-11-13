356c97dc7c5e5f82ac6d6e8008bbdd544a53d587
Nguyen Duy Tung <tata.nguyenduytung169@gmail.com>
2023-11-13 16:40:13 +0700

    13/11

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import {PostAuthor} from './PostAuthor';
+import { ReactionButtons } from "./ReactionButtons";
 
 export const SinglePostPage = ({match}) => {
     const {postId} = match.params;
@@ -24,6 +25,7 @@ export const SinglePostPage = ({match}) => {
                 <p className="post-content">{post.content}</p>
                 <PostAuthor userId={post.user} />
                 <br></br>
+                <ReactionButtons post={post} />
                 <Link to={`/editPost/${post.id}`} className="button">
                     Edit Post
                 </Link>
